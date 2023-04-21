2f5b0b53ab4561502c78ab670a551b909c784664
SicongShen0223 <sishen@ucsd.edu>
2023-04-21 16:12:44 -0700

    Task 4
    Task 4 First step: finding M

--- lung.m
+++ lung.m
@@ -2,6 +2,15 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-setup_lung
-cvsolve
-outchecklung
+
+% First step: change several M to find the maximum sustainble oxygen rate of
+% consumption (when the code stop)
+
+M_vec = 0:0.01:0.5;
+
+for i = 1:length(M_vec)
+    M = M_vec(i);
+    setup_lung
+    cvsolve
+    outchecklung
+end
